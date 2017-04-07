38768a5d2121b56c50a566fd08ac15543078f655
solomon256 <solokaso@gmail.com>
2017-04-07 09:29:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,19 @@ $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
 if ( ! empty ($messageText ))
-  {$answer=" invalid";
-if($messageText == "hi") {
-    $answer = "Hello";
+{
+$answer = "I don't understand. Ask me 'hi'." ;
+if ( $messageText == "hi" ) {
+$answer = "Hello" ;
 }
 $response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
+'recipient' => [ 'id' => $senderId ],
+'message' => [ 'text' => $answer ]
 ];
-$ch = curl_init('https://graph.facebook.com/v2.8/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_exec($ch);
-curl_close($ch);
-  } 
+$ch = curl_init ( 'https://graph.facebook.com/v2.8/me/messages?access_token=' . $accessToken );
+curl_setopt ( $ch , CURLOPT_POST , 1 );
+curl_setopt ( $ch , CURLOPT_POSTFIELDS , json_encode ( $response ));
+curl_setopt ( $ch , CURLOPT_HTTPHEADER , [ 'Content-Type: application/json' ]);
+curl_exec ( $ch );
+curl_close ($ch );
+}
