f91b303a490baa74343fee92a40876d80d4a3c16
solomon256 <solokaso@gmail.com>
2017-04-08 19:29:24 +0300

    Update valid1.php

--- valid1.php
+++ valid1.php
@@ -9,6 +9,6 @@ $validator = new SMTP_Validate_Email($email, $from);
 $smtp_results = $validator->validate();
  
 echo "<pre>",var_dump($smtp_results),"</pre>";
- echo $smtp_results;
+// echo $smtp_results;
 //$let=implode("array",$lett);
 ?>
