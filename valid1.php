71c1c40fe39b9280c0a834ab7feb98ef271fd800
solomon256 <solokaso@gmail.com>
2017-04-08 22:54:27 +0300

    Update valid1.php

--- valid1.php
+++ valid1.php
@@ -9,6 +9,6 @@ $validator = new SMTP_Validate_Email($email, $from);
 $smtp_results = $validator->validate();
  
 $list = "<pre>",var_dump($smtp_results),"</pre>";
- echo $list;
+ echo $list->domains;
 //cho  $smtp_results->email;
 ?>
