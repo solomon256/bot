436ab2e11be150eeb64426b9008cbfdb3c50c2db
solomon256 <solokaso@gmail.com>
2017-04-08 21:01:38 +0300

    Update valid1.php

--- valid1.php
+++ valid1.php
@@ -10,11 +10,9 @@ $validator = new SMTP_Validate_Email($email, $from);
 $smtp_results = $validator->validate();
 
 echo "<pre>",var_dump($smtp_results),"</pre>";
+ 
 
-ob_start();
-var_dump($smtp_results);
- $result = ob_get_clean();
-echo $myString = print_r($result, TRUE);
+echo $myString = print_r($smtp_results, TRUE);
 //echo  "KASONGA ".$myString;
  
 ?>
