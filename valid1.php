78b3d992f6a29699971e7a75faeabd543a1190a9
solomon256 <solokaso@gmail.com>
2017-04-08 19:07:16 +0300

    Update valid1.php

--- valid1.php
+++ valid1.php
@@ -3,7 +3,7 @@
 require('smtp-validate-email.php');
 
 $from = 'skasonga@cis.mak.ac.ug'; // for SMTP FROM:<> command
-$email = 'skasonga@pegasustechnologies.co.ug';
+$email = 'skasohtnga@pegasustechnologies.co.ug';
 
 $validator = new SMTP_Validate_Email($email, $from);
 $smtp_results = $validator->validate();
