dbf00cca8c062466a282b8d286fad147b1d6094b
solomon256 <solokaso@gmail.com>
2017-04-08 09:29:30 +0300

    Add files via upload

---
+++ valid1.php
@@ -0,0 +1,13 @@
+<?php
+
+require('smtp-validate-email.php');
+
+$from = 'skasonga@cis.mak.ac.ug'; // for SMTP FROM:<> command
+$email = 'solokaso@gmail.com';
+
+$validator = new SMTP_Validate_Email($email, $from);
+$smtp_results = $validator->validate();
+
+var_dump($smtp_results);
+
+?>
