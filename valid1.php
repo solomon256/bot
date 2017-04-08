5ff776319372e63b4d1b894995a697a1cf4a56db
solomon256 <solokaso@gmail.com>
2017-04-08 19:27:00 +0300

    Update valid1.php

--- valid1.php
+++ valid1.php
@@ -8,7 +8,7 @@ $email = 'skasohtnga@pegasustechnologies.co.ug';
 $validator = new SMTP_Validate_Email($email, $from);
 $smtp_results = $validator->validate();
  
-$lett= "<pre>",var_dump($smtp_results),"</pre>";
- echo $lett;
+echo "<pre>",var_dump($smtp_results),"</pre>";
+ echo $smtp_results;
 //$let=implode("array",$lett);
 ?>
