54a86bd294a13b8540e4cc8dc8ee3919518f6827
solomon256 <solokaso@gmail.com>
2017-04-10 16:41:48 +0300

    Update index.php
    new Solomon

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-//r//equire('smtp-validate-email.php');
 // parameters
 $hubVerifyToken = 'TOKEN12345bbaacc';
 $accessToken = "EAAIrEaAEYeABAKn8ZAf88b8OsSSZAhgVrM9s7QHTlYNuE5bONVce2rfsyswzbenf98nkLlatxaFgditzXMZBlZCNOuvt8vryM8G6JJfwXS4t12wt3GoFIzCMbId9tC0bUnM1rklJZC72oOYrZBzbcZANZCGINmk9zRrVuD5YXLxD6QZDZD";
-
+ 
 // check token at setup
 if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
@@ -11,16 +10,15 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 }
 // handle bot's anwser
 $input = json_decode(file_get_contents('php://input'), true);
-
+ 
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-
+ 
 if ( ! empty ($messageText ))
 {
- 
 $answer = "I don't understand. Ask me 'hi'." ;
 if ( $messageText == "hi" ) {
-$answer="yes solomon";
+$answer = "Hello" ;
 }
 $response = [
 'recipient' => [ 'id' => $senderId ],
