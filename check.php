ba5ab98263c74ebbd446c386ddcd5432f79e8091
solomon256 <solokaso@gmail.com>
2017-04-10 16:35:04 +0300

    Update check.php

--- check.php
+++ check.php
@@ -5,13 +5,13 @@ require_once("smtpvalidateclass.php");
 // the email to validate  
 $emails = array['sologskf@gmail.com'];  
 // an optional sender  
-$sender = 'user@example.com';  
+$sender = 'skasonga@cis.mak.ac.ug';  
 // instantiate the class  
 $SMTP_Valid = new SMTP_validateEmail();  
 // do the validation  
 $result = $SMTP_Valid->validate($emails, $sender);  
 // view results  
 var_dump($result);  
-echo $email.' is '.($result ? 'valid' : 'invalid')."\n";  
+//echo $email.' is '.($result ? 'valid' : 'invalid')."\n";  
   
 // send email?   
