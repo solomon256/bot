01a895698f4549d6a8e387471176a18b8eff6cfd
solomon256 <solokaso@gmail.com>
2017-04-10 19:24:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 // parameters
+
 $hubVerifyToken = 'TOKEN12345bbaacc';
 $accessToken = "EAAIrEaAEYeABAKn8ZAf88b8OsSSZAhgVrM9s7QHTlYNuE5bONVce2rfsyswzbenf98nkLlatxaFgditzXMZBlZCNOuvt8vryM8G6JJfwXS4t12wt3GoFIzCMbId9tC0bUnM1rklJZC72oOYrZBzbcZANZCGINmk9zRrVuD5YXLxD6QZDZD";
  
+require_once("smtpvalidateclass.php");
 // check token at setup
 if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
@@ -14,11 +16,29 @@ $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
  
+
+
+// the email to validate  
+$emails = array('solokaso@gmal.com');
+// an optional sender  
+$sender = 'skasonga@cis.mak.ac.ug';  
+// instantiate the class  
+$SMTP_Valid = new SMTP_validateEmail();  
+// do the validation  
+$result = $SMTP_Valid->validate($emails, $sender);  
+// view results  
+var_dump($result);  
+//$say= $email.' is '.($result ? 'valid' : 'invalid')."\n";  
+// $allKeys = array_keys($result);
+//$allValues = array_values($result);
+
+
+
 if ( ! empty ($messageText ))
 {
 $answer = "I don't understand. Ask me 'hi'." ;
 if ( $messageText == "hi" ) {
-$answer = "Hello" ;
+$answer = "Hello ,".$allKeys[0]." is ".$allValues[0]; ;
 }
 $response = [
 'recipient' => [ 'id' => $senderId ],
