0e5a5459fde25f4c9863348b09e0dcfbbb28dd05
solomon256 <solokaso@gmail.com>
2017-07-04 22:06:07 +0300

    Update botfb.php

--- botfb.php
+++ botfb.php
@@ -1,18 +1,7 @@
 <?php
-
-require( 'smtp-validate-email.php' );
+ 
+//require( 'smtp-validate-email.php' );
 $from = 'skasonga@cis.mak.ac.ug' ; // for SMTP FROM:<> command
 $email = 'someo@gmail.com' ;
-$validator = new SMTP_Validate_Email ( $email , $from );
-$result = $validator -> validate();
-var_dump ( $result);
-
-
-$allKeys = array_keys($result);
-$allValues = array_values($result);
-echo $allKeys[0]."\n is ".$allValues[0];
-echo $allKeys[1]." is ".$allValues[1];
-echo $allKeys[2]." is ".$allValues[2];
-echo $allKeys[3]." is ".$allValues[3];
-echo $allKeys[4]." is ".$allValues[4];
-echo $allKeys[5]." is ".$allValues[5];
+//$validator = new SMTP_Validate_Email ( $email , $from );
+echo  "200";
