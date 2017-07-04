aa22c283666b4d6a76c1af587ca20c9271d69e19
solomon256 <solokaso@gmail.com>
2017-07-04 21:46:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,8 @@
 <?php
 // parameters
-
-$hubVerifyToken = 'TOKEN12345bbaacc';
-$accessToken = "EAAIrEaAEYeABAKn8ZAf88b8OsSSZAhgVrM9s7QHTlYNuE5bONVce2rfsyswzbenf98nkLlatxaFgditzXMZBlZCNOuvt8vryM8G6JJfwXS4t12wt3GoFIzCMbId9tC0bUnM1rklJZC72oOYrZBzbcZANZCGINmk9zRrVuD5YXLxD6QZDZD";
- 
-require_once("smtpvalidateclass.php");
-// check token at setup
-if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
-  echo $_REQUEST['hub_challenge'];
-  exit;
-}
-// handle bot's anwser
-$input = json_decode(file_get_contents('php://input'), true);
- 
-$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
-$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
- 
-
-if ( ! empty ($messageText ))
-{
-$answer = "I don't understand. Ask me 'hi'." ;
-if ( $messageText != "" ) {
- 
-// $emails = array($messageText);
-// an optional sender  
-$sender = 'skasonga@cis.mak.ac.ug';  
-// instantiate the class  
-$SMTP_Valid = new SMTP_validateEmail();  
-// do the validation  
-$result = $SMTP_Valid->validate($messageText, $sender);  
-// view results  
-var_dump($result);  
-//$say= $email.' is '.($result ? 'valid' : 'invalid')."\n";  
- $allKeys = array_keys($result);
-$allValues = array_values($result);
+echo  "200"
  
  
-$answer = "Hello ,".$allKeys[0]." is ".$allValues[0]; ;
-}
-$response = [
-'recipient' => [ 'id' => $senderId ],
-'message' => [ 'text' => $answer ]
-];
-$ch = curl_init ( 'https://graph.facebook.com/v2.8/me/messages?access_token=' . $accessToken );
-curl_setopt ( $ch , CURLOPT_POST , 1 );
-curl_setopt ( $ch , CURLOPT_POSTFIELDS , json_encode ( $response ));
-curl_setopt ( $ch , CURLOPT_HTTPHEADER , [ 'Content-Type: application/json' ]);
-curl_exec ( $ch );
-curl_close ($ch );
-}
+ print_r($_SERVER);
+print_r($_POST);
+print_r($_GET);
