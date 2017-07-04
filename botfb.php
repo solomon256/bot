856c7c085d6b9b28bdb5144e9921e04a16add0d3
solomon256 <solokaso@gmail.com>
2017-07-04 22:09:40 +0300

    Update botfb.php

--- botfb.php
+++ botfb.php
@@ -1,7 +1,4 @@
 <?php
  
-//require( 'smtp-validate-email.php' );
-$from = 'skasonga@cis.mak.ac.ug' ; // for SMTP FROM:<> command
-$email = 'someo@gmail.com' ;
-//$validator = new SMTP_Validate_Email ( $email , $from );
-echo  $email;
+
+ 
